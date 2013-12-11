4efa7ae66488f11a069019dc414e17c176de1ca5
Lluis <lluisyast@gmail.com>
2013-12-11 19:42:06 +0100

    Sencillo ejemplo de llamadas a funciones escritas en ensamblador

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -1,7 +1,18 @@
 #include <WProgram.h>
 
+// Se incluye "C" para evitar el 'name mangling' de C++
+extern "C" int leaf_example(int, int, int, int);
+extern "C" uint32_t return_pc();
+
 void setup()
 {
+	Serial.begin(9600);
+	Serial.println(return_pc());
+	int res = leaf_example(3,4,5,6);
+	Serial.println(res,DEC);
+	res = leaf_example(6,5,4,3);
+	Serial.println(res,DEC);
+	Serial.println(return_pc());
 }
 
 void loop()
