801ccd2862e90811ec4c6090dee9c09730578fad
Lluis <lluisyast@gmail.com>
2013-12-11 20:04:03 +0100

    Añadida funcion 'fact'

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -3,6 +3,7 @@
 // Se incluye "C" para evitar el 'name mangling' de C++
 extern "C" int leaf_example(int, int, int, int);
 extern "C" uint32_t return_pc();
+extern "C" uint32_t fact(uint32_t);
 
 void setup()
 {
@@ -13,6 +14,7 @@ void setup()
 	res = leaf_example(6,5,4,3);
 	Serial.println(res,DEC);
 	Serial.println(return_pc());
+	Serial.println(fact(10));
 }
 
 void loop()
